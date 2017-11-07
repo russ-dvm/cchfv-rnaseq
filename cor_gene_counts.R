cee07255944158e791da3a9cc7fb1a4a7cba2252
Russell Fraser <rfrase03@uoguelph.ca>
2017-11-07 09:45:30 -0800

    Function creation
    Converted the for loop into a function that can be run over both cell lines. Data gets finished into a DF with rounded numbers.

--- cor_gene_counts.R
+++ cor_gene_counts.R
@@ -1,22 +1,56 @@
 library(tidyverse)
 library(ggpubr)
+library(plyr)
 
 
 hep <- read.csv("~/Dropbox/temp/gene_count_matrix.csv", h = T)
 huh <- read.csv("~/Dropbox/temp/huh_gene_counts.csv", h = T)
-a <- cor.test(hep$Lib_22, hep$Lib_23, method = "pearson")
-shapiro.test(hep$Lib_22)
 
-for (i in 2:ncol(hep)){
-  
-  print(cor(hep[,i], hep[,i+1], method = "spearman"))
-  print(colnames(hep)[i])
-  
+
+cor_fnc <- function(x, offset){ 
+  cor_list <- list()
+    for (i in 1:(ncol(x)-1)){
+    j <- i
+    i <- i + offset
+
+  if (i %% 3 == 0){
+    liba <- paste("^Lib_", i-2, "$", sep="")
+    libb <- paste("^Lib_", i-1, "$", sep="")
+    libc <- paste("^Lib_", i, "$", sep="")
+    
+    colLiba <- grep(liba, colnames(x))
+    colLibb <- grep(libb, colnames(x))
+    colLibc <- grep(libc, colnames(x))
+    
+    comp1 <- paste(liba, libb, sep="-")
+    comp1 <- gsub("\\^", "", comp1)
+    comp1 <- gsub("\\$", "", comp1)
+    cor_ab <- cor(x[,colLiba], x[,colLibb], method = "spearman")
+    
+    comp2 <- paste(liba, libc, sep="-")
+    comp2 <- gsub("\\^", "", comp2)
+    comp2 <- gsub("\\$", "", comp2)
+    cor_ac <- cor(x[,colLiba], x[,colLibc], method = "spearman")
+
+    comp3 <- paste(libb, libc, sep="-")
+    comp3 <- gsub("\\^", "", comp3)
+    comp3 <- gsub("\\$", "", comp3)
+    cor_bc <- cor(x[,colLibb], x[,colLibc], method = "spearman")
+
+    cor_list[[j-2]] <- c(comp1, cor_ab)
+    cor_list[[j-1]] <- c(comp2, cor_ac)
+    cor_list[[j]] <- c(comp3, cor_bc)
+  }
+  }
+  return(cor_list)
 }
 
-for (i in 2:ncol(huh)){
-  
-  print(cor(huh[,i], huh[,i+1], method = "spearman"))
-  print(colnames(huh)[i])
-  
-}
+hepList <- cor_fnc(hep, 18)
+hepDf <- ldply(hepList)
+hepDfRound <- hepDf 
+hepDfRound$V2 <- signif(as.numeric(hepDfRound$V2), digits = 3)
+
+huhList <- cor_fnc(huh, 0)
+huhDf <- ldply(huhList)
+huhDfRound <- huhDf
+huhDfRound$V2 <- signif(as.numeric(huhDfRound$V2), digits = 3)
