e7cb568baacfd3d50459c0b74a8905f6122ebb60
Russell Fraser <rfrase03@uoguelph.ca>
2019-02-06 09:57:54 -0500

    changed line order so script actually works...

--- summarize_reads.R
+++ summarize_reads.R
@@ -6,9 +6,11 @@ key <- read.table("~/cchf/ref_files/cchv_key.txt", h = T, sep = "\t")
 
 ## READS ALIGNED TO THE HUMAN GENOME
 aligned <- read.table("~/cchf/3_aligned/complete_summary.txt", h = F, sep = "\t")
+colnames(aligned) <- c("Aligner", "Library", "Metric", "Value")
+
+
 aligned <- merge(aligned, key[,c(1,2,3,5)], by.x = "Library", by.y = "library")
 
-colnames(aligned) <- c("Aligner", "Library", "Metric", "Value")
 
 ggplot(subset(aligned, aligned$Metric == "rate"), aes(x = cell_line, y = Value, fill = status)) + 
   geom_boxplot() 
